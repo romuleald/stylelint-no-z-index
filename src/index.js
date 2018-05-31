89e8668db376267d11bd8df3bb461a57d9327e9b
Antoine Sanchez <romuleald@users.noreply.github.com>
2018-05-31 15:11:42 +0200

    add variables

--- src/index.js
+++ src/index.js
@@ -2,18 +2,39 @@ import stylelint from 'stylelint'
 
 export const ruleName = 'plugin/no-z-index'
 export const messages = stylelint.utils.ruleMessages(ruleName, {
-  default: () => {
-    return 'You should not use numeric values for z-index.'
+    2: function _default() {
+    return 'You should not use numeric values for z-index.';
+  },
+  'only-variables': function _default() {
+    return 'You should use only variables for z-index.';
+  },
+  'all': function _default() {
+    return 'You should use only variables or not numeric values for z-index.';
   }
 })
+const defaultAllowedValue = ['auto', 'inherit', 'initial', 'unset'];
+const variableAllowedValue = ['^[@|$]'];
 
 module.exports = stylelint.createPlugin(ruleName, (options) => (cssRoot, result) => {
+  let allowedValue = [];
+  switch (options) {
+    case 2:
+      allowedValue = defaultAllowedValue;
+      break;
+    case 'only-variables':
+      allowedValue = variableAllowedValue;
+      break;
+    case 'all':
+      allowedValue = defaultAllowedValue.concat(variableAllowedValue);
+      break;
+  }
+
   cssRoot.walkDecls('z-index', (decl) => {
     // Check if the z-index value is one of the following non-numeric values
-    const allowedValue = ['auto', 'inherit', 'initial', 'unset']
     const value = decl.value
-    if (allowedValue.indexOf(value.toLowerCase()) >= 0) return
-
+    if (allowedValue.filter(item => new RegExp(item).test(value)).length) {
+      return;
+    }
     const cssVarRegex = /var\(.*\)/
     if (cssVarRegex.test(value)) return
 
@@ -21,7 +42,7 @@ module.exports = stylelint.createPlugin(ruleName, (options) => (cssRoot, result)
       ruleName: ruleName,
       result: result,
       node: decl,
-      message: messages.default()
+      message: messages[options]()
     })
   })
 })
